45d7acdf3ca8acace7b6b4193b8c43921af65947
buntun <tunza1939@hotmail.com>
2018-10-24 15:29:52 +0700

    my frist commit

---
+++ my_web_app/App_Code/WebService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod] // use this to declaer method on web service.
+    public string HelloWorld()
+    {
+        return "Hello World";
+    } //edit this function to modify web service
+    [WebMethod]
+    public string HelloKitty()
+    {
+        return "HelloKitty";
+    }
+}
